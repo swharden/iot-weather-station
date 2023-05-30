486318c067bdbd1373f1d232061c2a074a9299d2
Leah <leahxharden@gmail.com>
2023-05-30 12:53:41 -0400

    started Titles

--- src/frontend/src/Pages/Front.js
+++ src/frontend/src/Pages/Front.js
@@ -5,21 +5,29 @@
 
 function Front() {
     return (
-        <div className="bigGrid">
+        <div>
+            <div className="bigGrid Montserrat">
 
-            <h2 className="title">Weather Station</h2>
-            <div>
+                <h2 className="title">Weather Station:</h2>
+                <div>
+
+                </div>
+                <h2 className="title">Inside:</h2>
+                <div>
+
+                </div>
+                <h2 className="title">Outside:</h2>
+                <div>
+
+                </div>
+                <h2 className="title">Pressure:</h2>
+                <div>
+
+                </div>
 
             </div>
-            <h2>Inside</h2>
-            <div>
-                <h2>Outside</h2>
-                <div></div>
-            </div>
-            <h2>Pressure</h2>
-            <div></div>
+        </div >
 
-        </div>
     )
 }
 export default Front;

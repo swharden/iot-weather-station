df53d2a88229464cb277c9de498992520f0f91bb
Leah <leahxharden@gmail.com>
2023-05-31 13:25:41 -0400

    organized graphs

--- src/frontend/src/Pages/Modules/Graphs/GraphsData.js
+++ src/frontend/src/Pages/Modules/Graphs/GraphsData.js
@@ -15,23 +15,23 @@ function GraphData() {
                 <div />
 
                 <div class="my-5">
-                    <h3>Indoor</h3>
+
                     <canvas id="indoorChart" class="w-100"></canvas>
                 </div>
 
 
                 <div class="my-5">
-                    <h3>Outdoor</h3>
+
                     <canvas id="outdoorChart" class="w-100"></canvas>
                 </div>
 
                 <div class="my-5">
-                    <h3>Attic</h3>
+
                     <canvas id="atticChart" class="w-100"></canvas>
                 </div>
 
                 <div class="my-5">
-                    <h3>Pressure</h3>
+
                     <canvas id="pressureChart" class="w-100"></canvas>
                 </div>
             </div>
